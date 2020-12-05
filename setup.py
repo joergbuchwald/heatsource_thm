cea36b55ff61734714b923db0477ee90cdc744d8
Joerg Buchwald <joerg.buchwald@ufz.de>
2020-12-05 22:15:07 +0100

    intial commit v. 0.1

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""ogs6py: a python API for OpenGeoSys6"""
+
+from setuptools import setup, find_packages
+
+setup(name="heatsource.py",
+      version=0.1,
+      maintainer="Jörg Buchwald",
+      maintainer_email="joerg_buchwald@ufz.de",
+      author="Jörg Buchwald",
+      author_email="joerg.buchwald@ufz.de",
+      url="https://github.com/joergbuchwald/heatsource_Thm",
+      license="MIT -  see LICENSE.txt",
+      platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+      include_package_data=True,
+      install_requires=["numpy", "scipy"],
+      py_modules=["heatsource"],
+      packages=[])
